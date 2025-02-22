2b43dea6182a563d8aec0a68a2b2512e49f34419
Marnei Machado <marnei.machado@gmail.com>
2025-02-22 16:45:37 -0300

    feat(api): add service to get all pokemon name and id

---
+++ src/services/pokemon/index.ts
@@ -0,0 +1,31 @@
+const POKEMON_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+
+export async function getAllPokemon(offset = 0, limit = 20) {
+  const allPokemonUrl = `${POKEMON_API_URL}?offset=${offset}$limit=${limit}`
+  const res = await fetch(allPokemonUrl)
+
+  if (!res.ok) {
+    throw new Error('An error occurred while fetching the pokemon')
+  }
+  return res.json()
+}
+
+export async function getPokemonById(id: number) {
+  const res = await fetch(`${POKEMON_API_URL}${id}`)
+
+  if (!res.ok) {
+    throw new Error('An error occurred while fetching the pokemon')
+  }
+
+  return res.json()
+}
+
+export async function getPokemonByName(name: string) {
+  const res = await fetch(`${POKEMON_API_URL}${name}`)
+
+  if (!res.ok) {
+    throw new Error('An error occurred while fetching the pokemon')
+  }
+
+  return res.json()
+}
