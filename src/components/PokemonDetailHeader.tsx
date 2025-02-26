2916d02635f31f70b7666bfbdd83ce1787e51353
Marnei Machado <marnei.machado@gmail.com>
2025-02-26 20:05:33 -0300

    feat(ui): add pokeAPI logo at header

--- src/components/PokemonDetailHeader.tsx
+++ src/components/PokemonDetailHeader.tsx
@@ -7,11 +7,19 @@ type PokemonDetailHeaderProps = {
 
 export function PokemonDetailHeader({ pillClass }: PokemonDetailHeaderProps) {
   return (
-    <header className={`p-4 w-full ${pillClass}`}>
+    <header className={`p-4 w-full ${pillClass} flex flex-row justify-between`}>
       <Link to="/" className="font-bold flex flex-row items-center text-white">
         <ArrowLeft size={24} className="inline mr-2 text-white" weight="bold" />
         Back
       </Link>
+
+      <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">
+        <img
+          className="w-16"
+          src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+          alt="pokeapi"
+        />
+      </a>
     </header>
   )
 }
