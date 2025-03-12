6017ad4031108b130098811a05b1f86ec81eb2f7
Marnei Machado <marnei.machado@gmail.com>
2025-03-12 00:15:36 -0300

    config(deploy): add github page config deploy

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { PokemonDetailPage } from './pages/PokemonDetailPage'
 
 export function App() {
   return (
     <main className="min-h-svh bg-slate-100 pb-12">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pokemon/:identifier" element={<PokemonDetailPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </main>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: '/Pokedex/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
